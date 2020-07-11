32a92afe7906ffa99daf0f396b46ff464c1502fd
SNTag <SNTagore@protonmail.com>
2020-07-11 23:51:56 +0800

    moving to R

---
+++ stock-bot.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+### Author:
+## SNTag (SNTagore@protonmail.com)
+### Description:
+## Redoing my stock bot in R. It has become my main
+## language, and I envision a cleaner approach to data analysis with
+## it.
+##
+## This program will process stock-data, but differently from before.
+
+library(tidyverse)
+
