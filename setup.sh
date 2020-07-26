c222bcb69b311babd98951a3ebb9a97e4a1519b4
SNTag <SNTagore@protonmail.com>
2020-07-27 00:06:53 +0800

    adding a crontab script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@
 # Usage:
 # in the terminal, go to the directory bot.py is in.  Type 'bash setup.sh'.#setup.sh.
 
+script_name1="stock-bot.sh"
+script_path1=$(dirname $(readlink -f $0))
+script_path_with_name="$script_path1/$script_name1"
+echo $script_path_with_name
+crontab -l | grep -q "${script_path_with_name}"  && echo 'entry exists' || (crontab -l 2>/dev/null; echo "0 11 * * * ${script_path_with_name}") | crontab -
