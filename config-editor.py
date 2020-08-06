bf811f4af4adf228c9e62fc100a68b6462214e2c
SNTag <SNTagore@protonmail.com>
2020-08-06 18:52:48 +0800

    slack notification system

--- config-editor.py
+++ config-editor.py
@@ -22,19 +22,30 @@ def make_config():
     # notifier_system = str(input("enter the notifier system type here: "))
     # print("which notifier system will you use? \n note: the system you choose can be modified by editing the script 'notif.sh' \n -ifttt \n -email")
     echo "which notification system should I use?"
-    echo "- IFTTT"
-    echo "- slack"
+    echo "- 'IFTTT'"
+    echo "- 'slack'"
     approach = str(input("choose one of the above (HAS TO BE EXACT): "))
-    notifier_key = str(input("enter the notifier key here: "))
+
+    if [ approach == 'ifttt' ]; then
+        notifier_key = str(input("enter the notifier key here: "))
+    elif [ approach == 'slack' ]; then
+        echo "set up a slack webhook as needed. you should get a line as such:"
+        echo 'https://hooks.slack.com/services/${slack_key}'
+        echo "where slack_key looks like"
+        echo "afdjwe/asdfjhwe/djsfaklgrewuig"
+        notifier_key = str(input("enter what goes into slack_key: "))
+    else
+        echo "not sure how to send the notifications"
+    fi
     banned_length = str(input("how long do i keep tickers banned (in days): "))
 
     # generates a config file
-    f = open( './input/config.yaml', 'w' )
+    f = open( './input/config.yaml', 'w')
     f.write( 'api: ' + repr(api) + '\n' )
     f.write( 'api_key: ' + repr(api_key) + '\n' )
-    f.write( 'notifier_key: ' + repr(notifier_key) + '\n' )
     f.write( 'banned_length: ' + repr(banned_length) + '\n' )
     f.write( 'approach: ' + repr(approach) + '\n' )
+    f.write( 'notifier_key: ' + repr(notifier_key) + '\n' )
     f.close()
 
 
--- notify.sh
+++ notify.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ $3 == ifttt ]; then
+if [ $3 == 'ifttt' ]; then
     arg1=$(echo $1 | sed 's/ /%20/g')
     arg2=$(echo $2 | sed 's/ /%20/g')
     curl -s -o /dev/null "https://maker.ifttt.com/trigger/notify/with/key/${arg2}?value1=$arg1"
+elif [ $3 == 'slack' ]; then
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/${notifier_key}
 else
     echo "not sure how to send the notifications"
 fi
-
